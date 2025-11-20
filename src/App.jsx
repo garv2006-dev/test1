95dcda30f3eb8b35e4c8dbf636d6bab682cb5a6a
garv2006-dev <garvvariya03@gmail.com>
2025-11-20 13:14:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='my-8 text-center text-5xl font-bold'>hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/ProjectSidebar.jsx
@@ -0,0 +1,12 @@
+export default function ProjectSidebar() {
+  return (
+    <div className="w-64 h-full bg-gray-100 p-4">
+      <h2 className="text-xl font-bold mb-4">Project Sidebar</h2>
+      <ul>
+        <li className="mb-2">Project 1</li>
+        <li className="mb-2">Project 2</li>
+        <li className="mb-2">Project 3</li>
+      </ul>
+    </div>
+  )
+}
